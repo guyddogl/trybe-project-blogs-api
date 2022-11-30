e957ca18266f6f94b9a0b9941600a91144dfbf5c
Guyddo <guyddogl@gmail.com>
2022-11-30 20:25:19 -0300

    Add post routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const loginRoute = require('./routes/loginRoute');
 const userRoutes = require('./routes/userRoutes');
 const categoriesRoutes = require('./routes/categoriesRoutes');
+const postRoutes = require('./routes/postRoutes');
 // ...
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use('/login', loginRoute);
 app.use('/user', userRoutes);
 app.use('/categories', categoriesRoutes);
+app.use('/post', postRoutes);
 
 // ...
 
